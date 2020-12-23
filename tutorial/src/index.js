d68a1f481891c734eab9e90f1e78117d0a38ff93
Tina-Lightning <c.atherholt@gmail.com>
2020-12-23 10:30:01 -0500

    added CSS

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,12 +1,49 @@
-import React from "react";
+import { element } from "prop-types";
+import React, { Fragment } from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
+import "./index.css";
+
+// JSX Rules:
+// always return a single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+
+function BookList() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+    alt="book"
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617D98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Amelia Hepworth
+  </h4>
+);
+
+ReactDom.render(<BookList />, document.getElementById("root"));
