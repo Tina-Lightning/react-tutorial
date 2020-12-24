198d5a58a39ed2ffd675ef6c10793d424d6097a8
Tina-Lightning <c.atherholt@gmail.com>
2020-12-24 10:36:01 -0500

    intro to props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,22 @@
-import { element } from "prop-types";
-import React, { Fragment } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
+// CSS
 import "./index.css";
+
+// Set up variables
+const firstBook = {
+  author: "Amelia Hepworth",
+  title: "I Love You to the Moon and Back",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+};
+const secondBook = {
+  author: "Adam Wallace",
+  title: "How to Catch a Unicorn",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/717W6A80aVL._AC_UL200_SR200,200_.jpg",
+};
 
 // JSX Rules:
 // always return a single element
@@ -14,36 +28,37 @@ import "./index.css";
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          laboriosam, sed corporis voluptate blanditiis voluptates minima. Magni
+          perspiciatis tempore, iusto voluptatem veritatis omnis, consequatur ex
+          repudiandae, eligendi nam totam fuga!
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="book" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-    alt="book"
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617D98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Amelia Hepworth
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
