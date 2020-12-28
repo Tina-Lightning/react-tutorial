16e7ca0963512b6bc5aa63da591904c899441208
Tina-Lightning <c.atherholt@gmail.com>
2020-12-28 10:13:58 -0500

    event basics

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,18 +5,29 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // Set up variables
-const firstBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-};
-const secondBook = {
-  author: "Adam Wallace",
-  title: "How to Catch a Unicorn",
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/717W6A80aVL._AC_UL200_SR200,200_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    author: "Adam Wallace",
+    title: "How to Catch a Unicorn",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/717W6A80aVL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    author: "Ann Whitford Paul",
+    title: "If Animals Kissed Good Night",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+  },
+];
 
 // JSX Rules:
 // always return a single element
@@ -28,35 +39,33 @@ const secondBook = {
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-          laboriosam, sed corporis voluptate blanditiis voluptates minima. Magni
-          perspiciatis tempore, iusto voluptatem veritatis omnis, consequatur ex
-          repudiandae, eligendi nam totam fuga!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
     <article className="book">
       <img src={img} alt="book" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
+      <button type="button" onClick={clickHandler}>
+        example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        another example
+      </button>
     </article>
   );
 };
